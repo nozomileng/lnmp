8326d7a748d93a38e3e58c5f36ed58f21648e9f9
nozomileng <51223245+nozomileng@users.noreply.github.com>
2026-08-05 22:11:48 +0800

    Update redis.php

--- conf/redis.php
+++ conf/redis.php
@@ -25,4 +25,4 @@
     }
 
 ?>
-Redis Test tools for <a href="https://lnmp.org" target="_blank">LNMP一键安装包</a> <a href="https://bbs.vpser.net/forum-25-1.html" target="_blank">LNMP支持论坛</a>
+Redis Test tools for <a href="https://lnmp.me" target="_blank">LNMP一键安装包祛毒版</a> 
